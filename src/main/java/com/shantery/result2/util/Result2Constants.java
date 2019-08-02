5c1cda7e6708c7e1840382d2c6820ed97e86e611
m.takano <m.takano@NP-21-093-O.shantery.local>
2019-08-02 13:52:50 +0900

    川原さんからの贈り物

---
+++ src/main/java/com/shantery/result2/util/Result2Constants.java
@@ -0,0 +1,15 @@
+package com.shantery.result2.util;
+
+/**
+ * @author r.kawahara
+ * 共通　定数を保持するクラス
+ * 共通して使用する値はこのクラスで定義する。
+ */
+public class Result2Constants {
+
+	/** インスタンス生成禁止 **/
+	private Result2Constants() {}
+
+	/** スラッシュ **/
+	public static final String SLASH = "/";
+}
