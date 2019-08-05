6701b42e9be15cf33b71f6ab7b53988c81f7a7b7
k.teruya <k.teruya@NP-21-095-O.shantery.local>
2019-08-05 09:15:24 +0900

    2019/08/05 エラーのため再度リセット。 照屋

--- src/main/java/com/shantery/result2/Result2Repository.java
+++ src/main/java/com/shantery/result2/Result2Repository.java
@@ -4,9 +4,27 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface Result2Repository extends JpaRepository<Result2, Long> {
-	public List<Result2> findAllOrderByDate(Pageable pageable);
+	public List<Result2> rfindAllOrderByDate(Pageable pageable);
+
+	public List<Result2> rfindAllOrderByAgeASC(@Param("word")String sWord);
+
+	public List<Result2> rfindAllOrderByAgeDESC(@Param("word")String sWord);
+
+	public List<Result2> rfindAllOrderByCostASC(@Param("word")String sWord);
+
+	public List<Result2> rfindAllOrderByCostDESC(@Param("word")String sWord);
+
+	public List<Result2> rfindAllOrderByDateDESC(@Param("word")String sWord);
+
+	public List<Result2> rfindAllOrderByDateASC(@Param("word")String sWord);
+
+	public List<Result2> rfindAllOrderByDESC(@Param("word")String sWord);
+
+
+
 }
