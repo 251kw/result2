1f4ad6506467e01223d66efedae6e7fbec9aabda
k.fujimaki <k.fujimaki@NP-21-097-O.shantery.local>
2019-08-05 17:05:51 +0900

    2019/08/05 currentPageをUtilのメソッドに置き換えbyまっきー

--- src/main/java/com/shantery/result2/Result2Service.java
+++ src/main/java/com/shantery/result2/Result2Service.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import com.shantery.result2.paging.PagingUtil;
 import com.shantery.result2.paging.PagingView;
+import com.shantery.result2.util.Result2Util;
 
 @Service
 public class Result2Service {
@@ -21,7 +22,6 @@ public class Result2Service {
     private int recordPerPage;	// 1ページあたりの表示件数
 	@Value("${app.paginglength}")	// << < (1 2 3 4 5)←これの表示数 > >>
 	private int pagingLength;
-	//private static final String SESSION_FORM_ID="searchForm";	// キー
 
 	@Autowired
 	Result2Repository r2Repository;
@@ -30,29 +30,14 @@ public class Result2Service {
 
 	public List<Result2> find(String page, int recordPerPage) throws ParseException{
 
-		int currentPage = 1;	// 現在いるページ番号の初期化
-		if(page != null) {	// pageが存在するとき
-			try {
-				currentPage = Integer.parseInt(page);	// 現在いるページ番号を取る
-			} catch(NumberFormatException e) {
-				currentPage = 1;	// 失敗したら先頭にする
-			}
-		}
+		int currentPage = Result2Util.getCurrentPage(page);	// 現在いるページ番号の初期化
 		int offset = currentPage - 1; // 開始位置
 
 		return r2Repository.findAllOrderByDate(PageRequest.of(offset, recordPerPage));
 		}
 
 	public PagingView r2Paging(String sWord,String page) {
-		int currentPage = 1;	// 現在いるページ番号の初期化
-		if(page != null) {	// pageが存在するとき
-			try {
-				currentPage = Integer.parseInt(page);	// 現在いるページ番号を取る
-			} catch(NumberFormatException e) {
-				currentPage = 1;	// 失敗したら先頭にする
-			}
-		}
-		//int offset = currentPage - 1; // 開始位置
+		int currentPage = Result2Util.getCurrentPage(page);	// 現在いるページ番号の初期化
 		int totalRecordNum = count(sWord);
 
 		return PagingUtil.generatePagingView(
@@ -61,24 +46,10 @@ public class Result2Service {
 				recordPerPage,
 				pagingLength,
 				new HashMap<>());
-	}/*
-	public int count(String sWord) {	// データの総件数を返すメソッド
-		return r2Repository.countAll(sWord);
-	}*/
+	}
 
 	public List<Result2> search(String sWord, String page, int recordPerPage) throws ParseException{
-		int currentPage = 1;	// 現在いるページ番号の初期化
-		if(page != null) {	// pageが存在するとき
-			try {
-				currentPage = Integer.parseInt(page);	// 現在いるページ番号を取る
-			} catch(NumberFormatException e) {
-				currentPage = 1;	// 失敗したら先頭にする
-			}
-		}
-		//int totalRecordNum;	// データの総件数を取るための変数
-		//boolean flag = false;	// フリーワード検索を行っているどうかのフラグ
-			//flag = true;	// フラグを立てる
-			//int totalRecordNum = count2(sWord);	// 検索してヒットしたデータの総件数を取る
+		int currentPage = Result2Util.getCurrentPage(page);	// 現在いるページ番号の初期化
 		int offset = (currentPage-1);
 		return r2Repository.search(sWord,PageRequest.of(offset, recordPerPage));
 	}
