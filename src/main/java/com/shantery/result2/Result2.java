b2207004ba9037e5a303465851969c21ce60fd3e
m.takano <m.takano@NP-21-093-O.shantery.local>
2019-08-06 11:57:53 +0900

    bean追加 by高野

--- src/main/java/com/shantery/result2/Result2.java
+++ src/main/java/com/shantery/result2/Result2.java
@@ -27,6 +27,15 @@ public class Result2 implements Serializable {
 	private String subject;
 	private String text;
 	private String sender;
+	private String temp1;
+	private String temp2;
+	private String temp3;
+	private String temp4;
+	private String temp5;
+	private String temp6;
+	private String temp7;
+	private String temp8;
+	private String temp9;
 
 	public Result2(long long1, String string, String string2, String string3, String string4, String string5,
 			String string6, String string7, String string8, String string9, String string10) throws ParseException {
@@ -119,6 +128,60 @@ public class Result2 implements Serializable {
 	}
 	public void setSender(String sender) {
 		this.sender = sender;
+	}
+	public String getTemp1() {
+		return temp1;
+	}
+	public void setTemp1(String temp1) {
+		this.temp1 = temp1;
+	}
+	public String getTemp2() {
+		return temp2;
+	}
+	public void setTemp2(String temp2) {
+		this.temp2 = temp2;
+	}
+	public String getTemp3() {
+		return temp3;
+	}
+	public void setTemp3(String temp3) {
+		this.temp3 = temp3;
+	}
+	public String getTemp4() {
+		return temp4;
+	}
+	public void setTemp4(String temp4) {
+		this.temp4 = temp4;
+	}
+	public String getTemp5() {
+		return temp5;
+	}
+	public void setTemp5(String temp5) {
+		this.temp5 = temp5;
+	}
+	public String getTemp6() {
+		return temp6;
+	}
+	public void setTemp6(String temp6) {
+		this.temp6 = temp6;
+	}
+	public String getTemp7() {
+		return temp7;
+	}
+	public void setTemp7(String temp7) {
+		this.temp7 = temp7;
+	}
+	public String getTemp8() {
+		return temp8;
+	}
+	public void setTemp8(String temp8) {
+		this.temp8 = temp8;
+	}
+	public String getTemp9() {
+		return temp9;
+	}
+	public void setTemp9(String temp9) {
+		this.temp9 = temp9;
 	}
 
 
