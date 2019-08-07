2ccec4c4ed9a04af6880b5f69da151bdcc907b4e
k.teruya <k.teruya@NP-21-095-O.shantery.local>
2019-08-07 16:44:06 +0900

    2019/08/07 メソッドコメントを追加しました。照屋

--- src/main/java/com/shantery/result2/Result2Application.java
+++ src/main/java/com/shantery/result2/Result2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Result2Application {
 
+	//Spring Bootによるアプリケーションを起動するための処理
 	public static void main(String[] args) {
 		SpringApplication.run(Result2Application.class, args);
 	}
