116cee25dc5b209915b9c1f9b07674b9d84619d1
r.kawahara <r.kawahara@NP-21-099-S.shantery.local>
2019-08-07 15:57:00 +0900

    見出しの取得処理を追加

--- src/main/java/com/shantery/result2/util/Result2Util.java
+++ src/main/java/com/shantery/result2/util/Result2Util.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Value;
 
 import com.shantery.result2.Result2;
 
@@ -19,6 +22,9 @@ public class Result2Util {
 	private Result2Util() {}
 	/** ページ番号の初期値 **/
 	private static final String DEFAULT_PEGE = "1";
+	/** 表の表示列数 **/
+	@Value("${app.columnsize}")
+	private static int columnSize;
 
 	/**
 	 * 現在のページを返却します。
@@ -44,6 +50,17 @@ public class Result2Util {
 	}
 
 	/**
+	 * 表示する見出しを返却する
+	 * 列数はapplication.propertiesより取得
+	 * @return 表示する見出し
+	 */
+	public static List<String> getColumnName() {
+
+		String[] columnName = {"日時","国","雇","商","単金","年齢","最寄駅","L/S","件名","送信者"};
+		return Arrays.asList(columnName);
+	}
+
+	/**
 	 * DBから取得したListの内容を変換する（Result2型->List型）
 	 * 理由：Thymeleafで項目を可変にする為に実装
 	 * @param list DBから取得したリスト
@@ -54,6 +71,29 @@ public class Result2Util {
 		List<List<String>> listResult2 = new ArrayList<>(); // 表示内容がすべて入ったリスト
 		List<String>       result2     = new ArrayList<>(); // 1行分の表示内容
 
+		// Result2の内容をListに変換
+		/*result2 = list.stream().map(Result2 -> Result2.getId().toString()).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getDate).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getNationality).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getEmployment).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getCommercial_distribution).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getCost).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getAge).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getClosest_station).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getSubject).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getText).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getSender).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getTemp1).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getTemp2).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getTemp3).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getTemp4).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getTemp5).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getTemp6).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getTemp7).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getTemp8).collect(Collectors.toList());
+		result2 = list.stream().map(Result2::getTemp9).collect(Collectors.toList());*/
+
+
 		// サンプルデータ
 		String[] smp1 = {"1", "2019-07-08 15:52:32", "JPN", "派遣", "商流", "100", "28", "新宿駅", "件名", "本文", "田中"};
 		String[] smp2 = {"2", "2019-07-16 14:18:58", "USA", "常駐", "商流", "1500", "35", "Grand Central Station", "お腹すいた", "初めまして", "ボンジュール鈴木"};
@@ -61,9 +101,10 @@ public class Result2Util {
 		List<String> smpList1 = Arrays.asList(smp1);
 		List<String> smpList2 = Arrays.asList(smp2);
 		List<String> smpList3 = Arrays.asList(smp3);
-		listResult2.add(smpList1);
-		listResult2.add(smpList2);
-		listResult2.add(smpList3);
+		result2 = list.stream().map(Result2::getTemp9).collect(Collectors.toList());
+
+		listResult2.add(result2);
+		listResult2.add(result2);
 
 		// 表示内容がすべて入ったリストを返却
 		return listResult2;
