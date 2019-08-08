6fb601862cfff836117aa7e32589e630e9cffc4f
k.teruya <k.teruya@NP-21-095-O.shantery.local>
2019-08-08 10:45:01 +0900

    2019/08/08 メソッドコメント修正　 照屋

--- src/main/java/com/shantery/result2/Result2Repository.java
+++ src/main/java/com/shantery/result2/Result2Repository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface Result2Repository extends JpaRepository<Result2, Long> {
 	public List<Result2> findAllOrderByDate(Pageable pageable);
